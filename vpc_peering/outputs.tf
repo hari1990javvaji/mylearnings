510b838839e30678037111e94d03186747d1e5bb
hjavvaji <javvaji.hari.kishore@philips.com>
2023-10-17 20:45:25 +0530

    flask k8 mongodb crud

--- vpc_peering/outputs.tf
+++
@@ -1,5 +0,0 @@
-# -- vpc_peering/outputs.tf
-
-output "vpc_peering_id" {
-  value = aws_vpc_peering_connection.vpc_to_vpc.id
-}
--- vpc_peering/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "vpc_peering_name" {}
-
-variable "peer_vpc_id" {}
-
-variable "origin_vpc_id" {}
-
